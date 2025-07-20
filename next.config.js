34fc45fb16c590c766ad4a062eabf9c4927c4029
Angus Ryer <46163463+angusryer@users.noreply.github.com>
2025-07-19 23:20:32 -0400

    Update next.config.js to point /journal to gitlab pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+   async rewrites() {
+    return [
+      {
+        source: '/journal/:path*',
+        destination: 'https://voice-to-blog-947fc9.gitlab.io/:path*',
+      },
+      {
+        source: '/journal',
+        destination: 'https://voice-to-blog-947fc9.gitlab.io/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
