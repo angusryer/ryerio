3a30eb487db51407a7329345d522f089cb18b74a
Angus W. Ryer <gus@ryer.io>
2025-07-19 23:54:25 -0400

    Refactor Home page and update URL constants. Added journal link to the Home page and expanded URLS enum with additional links for LinkedIn, GitHub, email, GitLab, Twitter, and resume.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,34 +7,47 @@ import styles from "../styles/shared.module.css";
 import { URLS } from "../utils/constants";
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<Head>
-				<title>ryer.io</title>
-				<meta
-					name='description'
-					content='ryer.io is your CTO, but only when you need it'
-				/>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>ryer.io</title>
+        <meta name='description' content='ryer.io is your CTO, but only when you need it' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-			<main className={styles.main}>
-				<Image
-					width={128}
-					height={128}
-					src='/images/ryerio-logo256.png'
-					alt='ryer.io'
-				/>
-				<h1 className={styles.title}>ryer.io</h1>
-				<span className={styles.slogan}>Pay-as-you-go fractional CTO</span>
-				<Link href={URLS.calendar}>
-					<a className={styles.cta}>Book a free consultation</a>
-				</Link>
-			</main>
+      <main className={styles.main}>
+        <Image width={128} height={128} src='/images/ryerio-logo256.png' alt='ryer.io' />
+        <h1 className={styles.title}>ryer.io</h1>
+        <span className={styles.slogan}>Pay-as-you-go fractional CTO</span>
+        <Link style={{ marginTop: "1rem" }} href={URLS.calendar}>
+          Book a free consultation
+        </Link>
+        <Link href={URLS.journal}>
+          <span style={{ marginTop: "1rem" }}>Read my journals</span>
+        </Link>
+        <Link href={URLS.resume}>
+          <span style={{ marginTop: "1rem" }}>View my resume</span>
+        </Link>
+        <Link href={URLS.linkedin}>
+          <span style={{ marginTop: "1rem" }}>Connect with me on LinkedIn</span>
+        </Link>
+        <Link href={URLS.github}>
+          <span style={{ marginTop: "1rem" }}>View my GitHub</span>
+        </Link>
+        <Link href={URLS.gitlab}>
+          <span style={{ marginTop: "1rem" }}>View my GitLab</span>
+        </Link>
+        <Link href={URLS.twitter}>
+          <span style={{ marginTop: "1rem" }}>Follow me on Twitter</span>
+        </Link>
+        <Link href={URLS.email}>
+          <span style={{ marginTop: "1rem" }}>Email me</span>
+        </Link>
+      </main>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,10 @@
 export enum URLS {
-	calendar = "https://tidycal.com/ryerio"
+  calendar = "https://tidycal.com/ryerio",
+  linkedin = "https://www.linkedin.com/in/angusryer/",
+  github = "https://github.com/angusryer",
+  email = "mailto:gus@ryer.io",
+  gitlab = "https://gitlab.com/angusryer",
+  twitter = "https://x.com/angusryer",
+  journal = "https://ryer.io/journal",
+  resume = "https://ryer.io/resume",
 }
